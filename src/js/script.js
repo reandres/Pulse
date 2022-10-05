7e5c4441e0e9eadb439c4e1a02e02a1dcb38abbe
Andrey <reandres20000@gmail.com>
2022-10-06 00:05:04 +0300

    add catalog

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,38 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         speed: 1200,
-        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog_item__content').eq(i).toggleClass('catalog_item__content_active');
+                $('.catalog_item__list').eq(i).toggleClass('catalog_item__list_active');
+            });
+        });
+    }
+
+    toggleSlide('.catalog_item__link');
+    toggleSlide('.catalog_item__back');
 });
