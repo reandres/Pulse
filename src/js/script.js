445a247f1d2a81631773b50c1ed7de1401848bea
Andrey <reandres20000@gmail.com>
2022-10-11 00:06:14 +0300

    adopted 575px

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,4 @@
 $(document).ready(function() {
-    $('.carousel__inner').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 1200,
-        autoplay: true,
-        autoplaySpeed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
